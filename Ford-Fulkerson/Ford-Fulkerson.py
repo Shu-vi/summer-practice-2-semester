6d47d2ea8c2eb19d1b0ac226d8e3f62df671d23f
Vadim <vad.general@mail.ru>
2023-03-15 22:21:49 +0300

    Feat&fix: реализовать большую часть алгоритма, исправить небольшие недочёты

--- Ford-Fulkerson/Ford-Fulkerson.py
+++ Ford-Fulkerson/Ford-Fulkerson.py
@@ -16,22 +16,75 @@ class Graph:
         self.__source_node = source_node
         self.__stock_node = stock_node
 
+    #СДЕЛАНО
+    # Возвращаем мощность потока между вершинами по номерам вершин
+    def __get_flow_between_nodes(self, node_1=0, node_2=0) -> int:
+        for i in self.__transition_table:
+            if (i[0] == node_1) and (i[1] == node_2):
+                return i[2]
+
     # Находим случайный маршрут от истока к стоку и возвращаем список номеров вершин графа в той последовательности, в которой они идут в маршруте
     def __find_route(self) -> list:
-        pass
+        route = []
+        visited = set()
+        queue = [(self.__source_node, [self.__source_node])]
+        while queue:
+            (vertex, path) = queue.pop(0)
+            if vertex == self.__stock_node:
+                route = path
+                break
+            if vertex not in visited:
+                visited.add(vertex)
+                for transition in self.__transition_table:
+                    if transition[0] == vertex:
+                        queue.append((transition[1], path + [transition[1]]))
+        return route
 
+    #СДЕЛАНО
     # Находим минимальный поток в маршруте (в списке вершин)
     def __find_min_flow(self, route):
-        pass
+        min_flow = self.__get_flow_between_nodes(route[0], route[1])
+        for i in range(len(route) - 1):
+            flow = self.__get_flow_between_nodes(route[i], route[i+1])
+            if flow < min_flow:
+                min_flow = flow
+        return min_flow
 
+    #СДЕЛАНО
     # Вычитаем из маршрута минимальный поток маршрута
-    def __subtruct_min_from_route(self):
-        pass
+    def __subtruct_min_from_route(self, route, min_flow):
+        for i in range(len(route) - 1):
+            for j in self.__transition_table:
+                if j[0] == route[i] and j[1] == route[i+1]:
+                    j[2] -= min_flow
 
+    #СДЕЛАНО
+    # Вычисляет, можно ли двигаться из node_1 в node_2
+    def __is_can_move(self, node_1, node_2) -> bool:
+        return self.__get_flow_between_nodes(node_1, node_2) > 0
+
+    #СДЕЛАНО
     # Проверяем, есть ли ещё свободные маршруты из истока в сток
     def __is_has_routes(self) -> bool:
-        pass
+        visited = set()
+        stack = []
+        visited.add(self.__source_node)
+        stack.append(self.__source_node)
+        is_found_route = False
+        while stack:
+            is_found_node = False
+            for i in self.__transition_table:
+                if (i[0] == stack[len(stack) - 1]) and (not i[1] in visited) and self.__is_can_move(i[0], i[1]):
+                    visited.add(i[1])
+                    stack.append(i[1])
+                    is_found_node = True
+                    if i[1] == self.__source_node:
+                        return True
+            if not is_found_node:
+                stack.pop()
+        return is_found_route
 
+    #СДЕЛАНО
     def get_max_total_flow(self) -> int:
         max_total_flow = 0
         while self.__is_has_routes():
@@ -54,6 +107,6 @@ transition_table = [
     [5, 6, 13]
 ]
 
-g = Graph(transition_table)
+g = Graph(transition_table, 0, 6)
 
-print(g.get_max_total_flow())
+#print(g.__get_flow_between_nodes())
